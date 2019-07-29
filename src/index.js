d2856afa21138b09e83d363dbec953810f86bcf1
William5920 <1101044179@qq.com>
2019-07-29 18:34:51 +0800

    initial my vue-cms project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//项目的js入口文件
+
+import Vue from 'vue'
+import app from './App.vue'
+import { Header } from 'mint-ui';
+import './lib/mui/css/mui.min.css'
+
+Vue.component(Header.name, Header);
+
+
+var vm = new Vue({
+	el: '#app',
+	render: function(createElements) {
+		return createElements(app)
+	}
+})
