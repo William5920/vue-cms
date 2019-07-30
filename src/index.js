800de6c412b4135921c3f7a14386c7a4e00eaa3c
William5920 <1101044179@qq.com>
2019-07-30 17:09:46 +0800

    至此完成了首页的九宫格

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@
 
 import Vue from 'vue'
 import app from './App.vue'
-import { Header } from 'mint-ui';
-import './lib/mui/css/mui.min.css'
 
-Vue.component(Header.name, Header);
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import router from './router.js'
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+import { Header, Swipe, SwipeItem } from 'mint-ui';
+Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 
 var vm = new Vue({
 	el: '#app',
 	render: function(createElements) {
 		return createElements(app)
-	}
+	},
+	router
 })
